6d3d7f2175b37a19c0fc2eb6eaddad8303daadcd
Pascal Lauener <pascal@lauener.biz>
2020-01-18 18:31:31 +0100

    add jenkins add openldap add openldap ui

---
+++ install/jenkins/slave/Dockerfile
@@ -0,0 +1,18 @@
+FROM jenkinsci/jnlp-slave:latest
+
+USER root
+
+RUN apt-get update && \
+    apt-get install -qq -y --no-install-recommends \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg2 \
+      software-properties-common && \
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
+    apt-key fingerprint 0EBFCD88 && \
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" && \
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" && \
+    apt-get update && \
+    apt-get install -qq -y --no-install-recommends docker-ce && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
--- rancher/docker_desktop/ssl/self/create.sh
+++ rancher/docker_desktop/ssl/self/create.sh
@@ -30,10 +30,8 @@ emailAddress=starterkit@localhost
 "
 
 # Generate our Private Key, CSR and Certificate
-openssl genrsa -des3 -out "$SSL_DIR/rootCA.key"  -passout  pass:$PASSPHRASE 2048
-openssl req -subj "$(echo -n "$SUBJ1" | tr "\n" "/")" -x509 -new -nodes -key "$SSL_DIR/rootCA.key" -sha256 -days 1024  -out "$SSL_DIR/cacerts.pem" -passin pass:$PASSPHRASE
+# openssl genrsa -des3 -out "$SSL_DIR/rootCA.key"  -passout  pass:$PASSPHRASE 2048
+# openssl req -subj "$(echo -n "$SUBJ1" | tr "\n" "/")" -x509 -new -nodes -key "$SSL_DIR/rootCA.key" -sha256 -days 1024  -out "$SSL_DIR/cacerts.pem" -passin pass:$PASSPHRASE
 
 openssl req -new -nodes -subj "$(echo -n "$SUBJ2" | tr "\n" "/")" -out "$SSL_DIR/server.csr" -newkey rsa:2048 -keyout "$SSL_DIR/server.key"
 openssl x509 -req -in server.csr -CA "$SSL_DIR/cacerts.pem" -CAkey "$SSL_DIR/rootCA.key" -CAcreateserial -out "$SSL_DIR/server.crt" -days 500 -sha256 -extfile "$SSL_DIR/v3.ext" -passin pass:$PASSPHRASE
-
-kubectl -n ingress-nginx create secret tls ingress-default-cert --cert="$SSL_DIR/server.crt" --key="$SSL_DIR/server.key" -o yaml --dry-run=true > ingress-default-cert.yaml
