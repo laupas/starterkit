fe5b3f870c3dceccd9a7f430bd5d97391ee01b13
Pascal Lauener <pascal@lauener.biz>
2020-01-21 21:41:05 +0100

    update installer

---
+++ installer/Options.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using CommandLine;
+
+namespace Installer
+{
+    public class Options
+    {
+        [Option("task-overwrite", Required = false, HelpText = "If set, this tasks will be executed. !!!Some other options will be ignored. (task1 task2 taskn)")]
+        public IEnumerable<string> Tasks { get; set; }
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+        [Option('d', "dns", Required = false, HelpText = "Set DNS Name to be used.")]
+        public string Dns { get; set; }
+        [Option("ssl-hack", Required = false, HelpText = "Will reinstall the Certification chain for the used helm repos.", Default = false)]
+        public bool SslHack { get; set; }
+        [Option("install-rancher", Required = false, HelpText = "If true, Rancher will be installed on the defined Kubernetes Server", Default = true)]
+        public bool? InstallRancher { get; set; }
+        [Option("install-starterkit", Required = false, HelpText = "If true, Starterkit Applications will be installed on the defined Kubernetes Server", Default = true)]
+        public bool InstallStarterKit { get; set; }
+        [Option("dynamic-dns", Required = false, HelpText = "Which dynamic DNS Server should be used.", Default = "xip.io")]
+        public string DynamicDns { get; set; }
+    }
+}
--- installer/Program.cs
+++ installer/Program.cs
@@ -1,43 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
-using System.Threading;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 using static System.Console;
 using RestSharp;
 using System.Linq;
 using System.IO;
+using CommandLine;
 using installer;
-
+using System.Net.NetworkInformation;
 namespace Installer
 {
     static class Program
     {
-        static string rancherUrl = "starterkit.devops.family";
+        private static string rancherInternalUrl;
+        private static string rancherUrl;
+
         static void Main(string[] args)
         {
-            var ssl = "self";
-            
             ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            var options = SetOptions(args);
+            DefineTargets(options);
+            rancherUrl = $"{options.Dns}";
+            rancherInternalUrl = $"{options.Dns}";
+            var targets = DefineTargetsToBeExecuted(options);
+            if(targets != null)
+            {
+                RunTargetsAndExit(targets);
+            }
+        }
+        private static Options SetOptions(string[] args)
+        {
+            Options options = new Options();
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o => 
+                {
+                    WriteHeader("Command Line Options");
+                    System.Console.WriteLine($"Dns:               {o.Dns}");
+                    System.Console.WriteLine($"Verbose:           {o.Verbose}");
+                    System.Console.WriteLine($"InstallRancher:    {o.InstallRancher}");
+                    System.Console.WriteLine($"InstallStarterKit: {o.InstallStarterKit}");
+                    System.Console.WriteLine($"SslHack:           {o.SslHack}");
+                    System.Console.WriteLine($"DynamicDns:        {o.DynamicDns}");
+                    System.Console.WriteLine($"Tasks overwrite:   {String.Join(' ', o.Tasks)}");
+                    options = o;
+                })
+                .WithNotParsed(errorList => 
+                {
+                    errorList.ToList().ForEach(error => 
+                    {
+                        System.Console.WriteLine($"Error {error.Tag}");
+                    });
+                });
+            return options;
+        }
+        private static IEnumerable<string> DefineTargetsToBeExecuted(Options options)
+        {
+            if(options.Tasks == null || options.Tasks.Any() == true)
+            {
+                return options.Tasks;
+            }
+            
+            var targets = new List<string>();
+            if(options.SslHack)
+            {
+                targets.Add("installCertificates");
+            }
+            
+            targets.Add("checkKubeclt");
+            
+            if(options.InstallRancher == true)
+            {
+                targets.Add("installIngress");
+                targets.Add("installRancher");
+                targets.Add("configureRancher");
+            }
+            
+            if(options.InstallStarterKit)
+            {
+                targets.Add("installStarterKit");
+            }
+            
+            targets.AddRange(options.Tasks);
+            return targets;
+        }
+        
+        private static void DefineTargets(Options options)
+        {
+            var ssl = "self";
+            Target(
+                "default", 
+                DependsOn("checkKubeclt", "installIngress", "installRancher", "configureRancher", "installStarterKit"), 
+                () => { WriteLine($"default "); });
 
+            Target("installCertificates",  () =>
+            {
+                WriteHeader("Installing root CAs of helm repos");
+                var urls = new List<string>();
+                urls.Add("kubernetes-charts.storage.googleapis.com");
+                urls.Add("charts.jetstack.io");
+                urls.Add("releases.rancher.com");
+                foreach (var url in urls)
+                {
+                    File.WriteAllText("install.sh",
+                        $"openssl s_client -showcerts -verify 5 -connect " + url +
+                        ":443 < /dev/null | awk '/BEGIN/,/END/{ if(/BEGIN/){a++}; out=\"cert\"a\".crt\"; print >out}' && for cert in *.crt; do newname=$(openssl x509 -noout -subject -in $cert | sed -n 's/^.*CN=\\(.*\\)$/\\1/; s/[ ,.*]/_/g; s/__/_/g; s/^_//g;p').crt; mv $cert /usr/local/share/ca-certificates/$newname; done && update-ca-certificates");
+                    Run("bash", "install.sh");
+                }
+            });
             Target("checkKubeclt", () =>
             {
                 try
                 {
                     Run("kubectl", "get nodes");
-                    try 
+                    try
                     {
                         var hostAddress = Dns.GetHostEntry("host.docker.internal").AddressList.First().ToString();
                         WriteLine("Running inside Docker");
-                        WriteLine($"Add {hostAddress} {rancherUrl} to host");
-
-                        File.AppendAllText("/etc/hosts", $"{hostAddress} {rancherUrl}{Environment.NewLine}");
+                        WriteLine($"Add {hostAddress} {rancherInternalUrl} to host");
+                        File.AppendAllText("/etc/hosts", $"{hostAddress} {rancherInternalUrl}{Environment.NewLine}");
                     }
                     catch
                     {
-                            WriteLine("Running outside Docker");
+                        WriteLine("Running outside Docker");
                     }
                 }
                 catch
@@ -47,23 +133,19 @@ namespace Installer
                     throw;
                 }
             });
-
             Target("installIngress", DependsOn("checkKubeclt"), () =>
             {
-                Kubernetes.WriteHeader("Install Ingress");
-
+                WriteHeader("Install Ingress");
                 Kubernetes.InstallResourceIfNotExists("ingress-nginx", "namespace");
-                
                 Kubernetes.InstallResourceIfNotExists(
-                    "ingress-default-cert", 
-                    "secret tls", 
-                    "ingress-nginx", 
+                    "ingress-default-cert",
+                    "secret tls",
+                    "ingress-nginx",
                     $"--cert=./../ssl/{ssl}/server.crt --key=./../ssl/{ssl}/server.key ");
-
                 Kubernetes.InstallApplicationeIfNotExists(
-                    "ingress-nginx", 
-                    "stable/nginx-ingress", 
-                    "ingress-nginx", 
+                    "ingress-nginx",
+                    "stable/nginx-ingress",
+                    "ingress-nginx",
                     "--set controller.extraArgs.default-ssl-certificate=ingress-nginx/ingress-default-cert",
                     "deployment.apps/ingress-nginx-nginx-ingress-controller",
                     "deployment.apps/ingress-nginx-nginx-ingress-default-backend");
@@ -71,87 +153,103 @@ namespace Installer
             
             Target("installRancher", DependsOn("checkKubeclt"), () =>
             {
-                Kubernetes.WriteHeader("Install Rancher");
-                
+                WriteHeader("Install Rancher");
                 Kubernetes.InstallResourceIfNotExists("cattle-system", "namespace");
-                
                 Kubernetes.InstallResourceIfNotExists(
-                    "tls-rancher-ingress", 
-                    "secret tls", 
+                    "tls-rancher-ingress",
+                    "secret tls",
                     "cattle-system",
                     $"--cert=./../ssl/{ssl}/server.crt --key=./../ssl/{ssl}/server.key ");
-
                 Kubernetes.InstallResourceIfNotExists(
-                    "tls-ca", 
-                    "secret generic", 
+                    "tls-ca",
+                    "secret generic",
                     "cattle-system",
                     $"--from-file=./../ssl/{ssl}/cacerts.pem");
-                
                 Kubernetes.InstallApplicationeIfNotExists(
-                    "rancher", 
-                    "rancher-latest/rancher", 
-                    "cattle-system", 
-                    $"--set hostname={ rancherUrl } " +
+                    "rancher",
+                    "rancher-latest/rancher",
+                    "cattle-system",
+                    $"--set hostname={rancherInternalUrl} " +
                     "--set privateCA=true " +
                     "--set ingress.tls.source=secret",
                     "deploy/rancher");
-
+                Run("kubectl", "apply -f ./../components/rancher/rancher.ingress.yml");
             });
             
             Target("configureRancher", DependsOn("checkKubeclt"), () =>
             {
-                Kubernetes.WriteHeader("Configure Rancher");
-                var client = new RestClient($"https://{rancherUrl}");
-
+                WriteHeader("Configure Rancher");
+                var client = new RestClient($"https://{rancherInternalUrl}");
                 Rancher.WaitUntilIsUpAndReady(client);
                 var token = Rancher.Login(client);
                 Rancher.ChangePassword(client, token);
-                Rancher.SetServerUrl(client, rancherUrl);
+                Rancher.SetServerUrl(client, rancherInternalUrl);
             });
-
             Target("installStarterKit", DependsOn("checkKubeclt"), () =>
             {
-                Kubernetes.WriteHeader("Install Starterkit");
-
+                WriteHeader("Install Starterkit");
                 Kubernetes.InstallResourceIfNotExists("starterkit", "namespace");
-                
                 Run("kubectl", "apply -f ./../components/common/cert.yaml");
-
                 Kubernetes.InstallApplicationeIfNotExists(
-                    "ldap", 
-                    "stable/openldap", 
-                    "starterkit", 
+                    "ldap",
+                    "stable/openldap",
+                    "starterkit",
                     $"-f ./../components/openldap/values.yaml",
                     "deploy/ldap-openldap");
-
                 Kubernetes.InstallApplicationeIfNotExists(
-                    "ldap-ui", 
-                    "", 
-                    "starterkit", 
+                    "ldap-ui",
+                    "",
+                    "starterkit",
                     $"./../components/openldapui",
                     "deploy/ldap-ui-openldapui");
-
                 Kubernetes.InstallResourceIfNotExists(
-                    "starterkit", 
-                    "configmap", 
-                    "starterkit", 
+                    "starterkit",
+                    "configmap",
+                    "starterkit",
                     "--from-file=./../components/jenkins/config");
-
                 Kubernetes.InstallApplicationeIfNotExists(
-                    "jenkins", 
-                    "stable/jenkins", 
-                    "starterkit", 
+                    "jenkins",
+                    "stable/jenkins",
+                    "starterkit",
                     $"-f ./../components/jenkins/values.yaml --set master.ingress.hostName=jenkins.{rancherUrl}",
                     "deploy/jenkins");
-                
             });
-
-            Target("default", DependsOn("checkKubeclt", "installIngress", "installRancher", "configureRancher", "installStarterKit"), () =>
+        }
+        private static string GetRancherInternalUrl(Options options)
+        {
+            if(! string.IsNullOrEmpty(options.Dns))
             {
-                WriteLine("default");
-            });
-            
-            RunTargetsAndExit(args);
+                System.Console.WriteLine($"Using {options.Dns} as base url");
+                return options.Dns;
+            }
+            else
+            {
+                var list = new List<string>();
+                list.Add("10.0.75.1");
+                list.Add("192.168.65.1");
+                foreach (var ip in list)
+                {
+                    System.Console.WriteLine($"Try to ping {ip}");
+                    var ping = new Ping();
+                    var result = ping.Send(ip);
+                    if(result.Status == IPStatus.Success)
+                    {
+                        var url = $"starterkit.{result.Address}.{options.DynamicDns}";
+                        System.Console.WriteLine($"Using {url} as base url");
+                        return url;
+                    }
+                    
+                }
+            }
+                
+            throw new Exception("Script was not able to find a ip to be used. Please set it manually with the --dns flag");
+        }
+        
+        public static void WriteHeader(string name)
+        {
+            WriteLine("####################################");
+            WriteLine(name);
+            WriteLine("####################################");
         }
     }
 }
